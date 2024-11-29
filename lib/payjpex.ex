a67760dbb6ed68b1436601659e42fcba786c5812
Yoosuke <yoosukenakao@thewaggle.co.jp>
2024-11-29 18:27:28 +0900

    first commit

---
+++ lib/payjpex.ex
@@ -0,0 +1,18 @@
+defmodule Payjpex do
+  @moduledoc """
+  Documentation for `Payjpex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Payjpex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/payjpex_test.exs
@@ -0,0 +1,8 @@
+defmodule PayjpexTest do
+  use ExUnit.Case
+  doctest Payjpex
+
+  test "greets the world" do
+    assert Payjpex.hello() == :world
+  end
+end
